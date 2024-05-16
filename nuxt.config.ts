fc1ccd235007db7dc7c77e67338401fd93215535
Stew <ghost.pizza@gmail.com>
2024-05-16 11:32:01 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
- 
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   postcss: {
