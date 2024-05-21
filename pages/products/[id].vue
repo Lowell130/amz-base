6fd066bd19ebc6ea4c9bb4fc33c3e14120c83b1c
Stew <ghost.pizza@gmail.com>
2024-05-21 10:56:30 +0200

    Update [id].vue

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,9 +1,19 @@
 <template>
   <div class="px-6 py-10">
     <!-- <p>id product: {{ id }}</p> -->
-    <div @click="router.go(-1)" class="cursor-pointer">
+    <!-- <button @click="router.go(-1)" class="cpx-4 h-10 me-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
       <p>Back</p>
-    </div>
+    </button> -->
+
+    <button @click="router.go(-1)" class="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+          </svg>
+          Back
+        </button>
+
+
 
     <div v-if="pending">
     <p>loading...</p>
