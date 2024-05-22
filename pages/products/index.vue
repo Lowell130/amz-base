686f6202321ae1b71c820cab88f5be2e0c0c2ccc
Stew <ghost.pizza@gmail.com>
2024-05-22 13:16:59 +0200

    schede video

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
-        <a class="inline-block text-black hover:text-gray-600" href="/products/schede-madri/">Schede Madri</a>
+        <ul>
+        <li><a class="inline-block text-black hover:text-gray-600" href="/products/schede-madri/">Schede Madri</a></li>
+      <li>  <a class="inline-block text-black hover:text-gray-600" href="/products/schede-video/">Schede Video</a></li>
+    </ul>
     </div>
 </template>
