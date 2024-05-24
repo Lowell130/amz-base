463eae1845ebc7f9bb9bb22477835d8d0fb4a700
Stew <ghost.pizza@gmail.com>
2024-05-24 12:42:07 +0200

    try

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <ul>
-        <li><a class="inline-block text-black hover:text-gray-600" href="/products/schede-madri/">Schede Madri</a></li>
-      <li>  <a class="inline-block text-black hover:text-gray-600" href="/products/schede-video/">Schede Video</a></li>
+        <li><a class="inline-block text-black hover:text-gray-600" href="/products/schedemadri/">Schede Madri</a></li>
+      <li>  <a class="inline-block text-black hover:text-gray-600" href="/products/schedevideo/">Schede Video</a></li>
     </ul>
     </div>
 </template>
