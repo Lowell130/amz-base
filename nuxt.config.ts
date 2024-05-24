0cb5389c4a5553c61cd05a4737279c7bb8bdcb4c
Stew <ghost.pizza@gmail.com>
2024-05-24 12:00:37 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    static: true,
-  },
+  // nitro: {
+  //   static: true,
+  // },
 
   site: {
     url: 'https://example.com',
