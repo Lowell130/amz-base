663ce06027aae2d0e1b4bf91d298a4d85154d40a
Stew <ghost.pizza@gmail.com>
2024-05-24 12:57:34 +0200

    Update index.vue

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <ul>
-        <li><a class="inline-block text-black hover:text-gray-600" href="/products/schedemadri/">Schede Madri</a></li>
-      <li>  <a class="inline-block text-black hover:text-gray-600" href="/products/schedevideo/">Schede Video</a></li>
+        <li>
+   <a href="/products/schedevideo">schedeVideo</a>
+        </li>
+    
     </ul>
     </div>
 </template>
