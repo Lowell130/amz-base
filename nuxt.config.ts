398499833e88220f529d40b66bd156af522babce
Stew <ghost.pizza@gmail.com>
2024-06-01 22:04:47 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,12 @@ export default defineNuxtConfig({
   },
   
   site: {  
-    url: 'https://eidosinformatica.it',
+  url: 'https://eidosinformatica.it',
     name: 'Eidos Informatica',
     description: 'Migliori prezzi e recensioni per prodotti di informatica, tablet, portatili, componenti e altro ancora',
     defaultLocale: 'it', // not needed if you have @nuxtjs/i18n installed    
   },
   modules: ['@nuxtjs/seo'],
-  seo: {
-    meta: [
-      { name: 'google-site-verification', content: 'Q-VK_2Se54rJT10RvyOYgD3xX9uieENzzQ7vPsh64F0' }
-    ]
-  },
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   postcss: {
