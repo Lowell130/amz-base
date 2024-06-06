06f11251cd53e5f1f701f27eb222af379468b3bd
Stew <ghost.pizza@gmail.com>
2024-06-06 11:30:11 +0200

    added affiliate links

--- components/productC.vue
+++ components/productC.vue
@@ -38,7 +38,7 @@
               productc.price
             }}</span>
             <a
-              href="#"
+            target="_blank" :href="productc.AffiateLink + `nuxt-dev-21`"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >Acquista</a
             >
@@ -80,6 +80,7 @@ const truncateText = (text, maxLength) => {
         asin: productc.ASIN,
         price: productc.Price,
         rank: productc.SalesRank,
+        AffiateLink: productc.AffiateLink
       }));
     },
   }
--- components/productD.vue
+++ components/productD.vue
@@ -39,7 +39,7 @@
           productd.price
         }}</span>
         <a
-          href="#"
+        target="_blank" :href="productd.AffiateLink + `nuxt-dev-21`"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >Acquista</a
         >
@@ -80,6 +80,7 @@ const {  data: portatiliProducts } = useFetch(
         asin: productd.ASIN,
         price: productd.Price,
         rank: productd.SalesRank,
+        AffiateLink: productd.AffiateLink
       }));
     },
   }
